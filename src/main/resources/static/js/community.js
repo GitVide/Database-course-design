4aaa447c356df3dd433a9f1ce74d6f262f4a30a3
GitVide <1162065497@qq.com>
2019-12-14 22:09:50 +0800

    图标

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -33,7 +33,7 @@ function comment2target(targetId, type, content) {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
                     if (isAccepted) {
-                        window.open("https://github.com/login/oauth/authorize?client_id=2859958f9f059979ed3a&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
+                        window.open("https://github.com/login/oauth/authorize?client_id=83e7ee42167f84d10160&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
                         window.localStorage.setItem("closable", true);
                     }
                 } else {
