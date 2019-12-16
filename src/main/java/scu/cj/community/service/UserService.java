585120083c91b0175da0e56a3030519a7f789014
GitVide <1162065497@qq.com>
2019-12-17 01:12:21 +0800

    add

--- src/main/java/scu/cj/community/service/UserService.java
+++ src/main/java/scu/cj/community/service/UserService.java
@@ -1,7 +1,9 @@
 package scu.cj.community.service;
 
+import scu.cj.community.dto.UserDTO;
 import scu.cj.community.exception.CustomizeErrorCode;
 import scu.cj.community.exception.CustomizeException;
+import scu.cj.community.mapper.UserExtMapper;
 import scu.cj.community.mapper.UserMapper;
 import scu.cj.community.model.User;
 import scu.cj.community.model.UserExample;
@@ -9,12 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import scu.cj.community.utils.MD5Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private UserExtMapper userExtMapper;
 
     public void createOrUpdate(User user) {
         UserExample userExample = new UserExample();
@@ -79,5 +84,19 @@ public class UserService {
 
     public int updateUserInfo(String name,String password,String bio,Long id){
         return  userMapper.updateUserInfo(name,password,bio,id);
+    }
+    public void modifyUser(User user) {
+        UserExample example = new UserExample();
+        example.createCriteria()
+                .andIdEqualTo(user.getId());
+        userMapper.updateByExampleSelective(user, example);
+    }
+
+    public List<User> findUserBySearch(String search) {
+        List<User> userList = new ArrayList<>();
+        UserDTO userDto = new UserDTO();
+        userDto.setSearch(search);
+        userList = userExtMapper.selectBySearch(userDto);
+        return userList;
     }
 }
